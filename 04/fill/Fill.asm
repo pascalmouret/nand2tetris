f139d0a49d38d9ccf6b22d82859057162faab97a
Pascal Mouret <pascal.mouret@avrios.com>
2018-09-27 13:38:46 +0200

    fill screen

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -11,4 +11,55 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+	@R0
+	M=0
+	@SCREEN
+	D=A
+	@8192
+	D=D+A
+	@screen_max
+	M=D
+(RESET)
+	@R0
+	D=M
+	@color
+	M=D
+	@SCREEN
+	D=A
+	@screen_pos
+	M=D-1 // since we start with +1 when painting
+(LOOP)
+	// set current color
+	@KBD
+	D=M
+	@BODY
+	D;JEQ
+	D=1
+(BODY)
+	@R0
+	M=D
+	// check if we need to reset
+	@color
+	D=D-M
+	@RESET
+	D;JNE
+	// check if we need to paint
+	@screen_max
+	D=M
+	@screen_pos
+	D=D-M
+	@LOOP
+	D;JEQ
+	@color
+	D=M
+	@PAINT
+	D;JEQ
+	D=-1
+(PAINT)
+	@screen_pos
+	M=M+1
+	A=M
+	M=D
+	@LOOP
+	0;JMP
+	
